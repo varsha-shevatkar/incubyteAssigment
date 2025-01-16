9171ea702f0f3aadd21071c7e735874657adc43b
varsha-shevatkar <shevatkarv@gmail.com>
2025-01-16 10:53:26 +0530

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        
+        <style>
+           
+        </style>
+    </head>
+    <body class="antialiased">
+       <h1>hi</h1>
+    </body>
+</html>
